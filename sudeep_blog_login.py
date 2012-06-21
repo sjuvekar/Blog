17f0492365b22ee0bf29bb5c10fc18b534ba3307
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-21 10:09:51 -0400

    Error handling in login

--- sudeep_blog_login.py
+++ sudeep_blog_login.py
@@ -1,5 +1,16 @@
 from sudeep_blog_template import SudeepBlogTemplate
 
+__SUDEEP_BLOG_LOGIN_HTML__ = "sudeep_blog_login.html"
+
 class SudeepBlogLoginHandler(SudeepBlogTemplate):
 	def get(self):
-		self.render("sudeep_blog_login.html")
+		self.render(__SUDEEP_BLOG_LOGIN_HTML__, sudeep_blog_login_error = "")
+
+	def post(self):
+		username = self.request.get("username")
+		password = self.request.get("password")
+		if username == "sjuvekar" and password == "speakfriendandenter":
+			self.response.headers.add_header("Set-Cookie", "user_id=u")
+			self.redirect("/blog")
+		else:
+			self.render(__SUDEEP_BLOG_LOGIN_HTML__, sudeep_blog_login_error = "Incorrect Username and/or Password")
