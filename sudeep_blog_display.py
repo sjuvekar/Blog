e7a4113e119c2e376038e6b467ca67c39746b50f
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-21 01:31:24 -0400

    Adding login link to display page

--- sudeep_blog_display.py
+++ sudeep_blog_display.py
@@ -8,4 +8,8 @@ __SUDEEP_BLOG_DISPLAY_HTML__ = "sudeep_blog_display.html"
 class SudeepBlogDisplayHandler(SudeepBlogTemplate):
 	def get(self):
 		blog_entries = db.GqlQuery("SELECT * FROM SudeepBlogDB ORDER BY sudeep_blog_created DESC")
-		self.render(__SUDEEP_BLOG_DISPLAY_HTML__, blog_entries = blog_entries)
+		sudeep_blog_login = "login"
+		user_id = self.request.cookies.get("user_id")
+		if user_id:
+			sudeep_blog_login = "logout"
+		self.render(__SUDEEP_BLOG_DISPLAY_HTML__, blog_entries = blog_entries, sudeep_blog_login = sudeep_blog_login)
