4e9a325bdee666503f73c526345bf98f474da602
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-21 10:50:28 -0400

    Login validation complete except setting cookie part

--- sudeep_blog_login.py
+++ sudeep_blog_login.py
@@ -2,6 +2,8 @@ from sudeep_blog_template import SudeepBlogTemplate
 from sudeep_blog_user_db import SudeepBlogUserDB
 import sudeep_blog_hasher 
 
+from google.appengine.ext import db
+
 __SUDEEP_BLOG_LOGIN_HTML__ = "sudeep_blog_login.html"
 
 class SudeepBlogLoginHandler(SudeepBlogTemplate):
@@ -11,7 +13,9 @@ class SudeepBlogLoginHandler(SudeepBlogTemplate):
 	def post(self):
 		username = self.request.get("username")
 		password = self.request.get("password")
-		if sudeep_blog_hasher.validate(username, password, "41b5194bbb126d47996c7d0678e1c526951801b3e75b416f4212449090c35f16|wolYapuTbK"):
+		user_query = db.GqlQuery("SELECT * FROM SudeepBlogUserDB WHERE sudeep_blog_db_username IN ('{0}')".format(username))
+		hash_password_record = user_query.get()
+		if hash_password_record and sudeep_blog_hasher.validate(username, password, hash_password_record.sudeep_blog_db_password):
 			self.response.headers.add_header("Set-Cookie", "user_id=u")
 			self.redirect("/blog")
 		else:
