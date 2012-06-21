0c986a4bd4fca52e160028a2fa4f0dc868633703
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-21 10:31:25 -0400

    basic input validation

--- sudeep_blog_login.py
+++ sudeep_blog_login.py
@@ -1,4 +1,6 @@
 from sudeep_blog_template import SudeepBlogTemplate
+from sudeep_blog_user_db import SudeepBlogUserDB
+import sudeep_blog_hasher 
 
 __SUDEEP_BLOG_LOGIN_HTML__ = "sudeep_blog_login.html"
 
@@ -9,7 +11,7 @@ class SudeepBlogLoginHandler(SudeepBlogTemplate):
 	def post(self):
 		username = self.request.get("username")
 		password = self.request.get("password")
-		if username == "sjuvekar" and password == "speakfriendandenter":
+		if sudeep_blog_hasher.validate(username, password, "41b5194bbb126d47996c7d0678e1c526951801b3e75b416f4212449090c35f16|wolYapuTbK"):
 			self.response.headers.add_header("Set-Cookie", "user_id=u")
 			self.redirect("/blog")
 		else:
