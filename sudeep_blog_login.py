591356140e16a1fec4470ccc84a2f31bc7e95882
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-24 15:48:34 -0400

    Getting rid of GqlQuery and using App Engine's Query class

--- sudeep_blog_login.py
+++ sudeep_blog_login.py
@@ -11,11 +11,11 @@ class SudeepBlogLoginHandler(SudeepBlogTemplate):
 		self.render(__SUDEEP_BLOG_LOGIN_HTML__, sudeep_blog_login_error = "")
 
 	def post(self):
-		username = self.request.get("username")
-		password = self.request.get("password")
-		user_query = db.GqlQuery("SELECT * FROM SudeepBlogUserDB WHERE sudeep_blog_db_username IN ('{0}')".format(username))
+		sudeep_blog_username = self.request.get("sudeep_blog_username")
+		sudeep_blog_password = self.request.get("sudeep_blog_password")
+		user_query = SudeepBlogUserDB.all().filter("sudeep_blog_db_username = ", sudeep_blog_username)
 		hash_password_record = user_query.get()
-		if hash_password_record and sudeep_blog_hasher.validate(username, password, hash_password_record.sudeep_blog_db_password):
+		if hash_password_record and sudeep_blog_hasher.validate(sudeep_blog_username, sudeep_blog_password, hash_password_record.sudeep_blog_db_password):
 			self.response.headers.add_header("Set-Cookie", "user_id={0}|{1}; Path=/".format(hash_password_record.key().id(), hash_password_record.sudeep_blog_db_password))
 			self.redirect("/blog")
 		else:
