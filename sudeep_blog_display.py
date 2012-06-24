a5fb2044100ecef8d82969f3edc05b95854e24a8
Sudeep Juvekar <sjuvekar@gmail.com>
2012-06-24 16:23:24 -0400

    passing page as a GET parameter to display to show 10 relevant results

--- sudeep_blog_display.py
+++ sudeep_blog_display.py
@@ -5,13 +5,22 @@ import sudeep_blog_hasher
 from google.appengine.ext import db
 
 __SUDEEP_BLOG_DISPLAY_HTML__ = "sudeep_blog_display.html"
+__NUM_ENTRIES_PER_PAGE__ = 10
 
 class SudeepBlogDisplayHandler(SudeepBlogTemplate):
 	def head(self):
 		pass
 	
 	def get(self):
-		blog_entries = SudeepBlogDB.all().order("-sudeep_blog_created")
+		page = self.request.get("page")
+		if not page or not page.isdigit():
+			page = 1
+		page = int(page)
+	 	num_entries_to_skip = (page - 1) * __NUM_ENTRIES_PER_PAGE__
+		if num_entries_to_skip >= SudeepBlogDB.all().count():
+			page = 1
+			num_entries_to_skip = 0	
+		blog_entries = SudeepBlogDB.all().order("-sudeep_blog_created").fetch(limit = __NUM_ENTRIES_PER_PAGE__, offset = num_entries_to_skip)
 		sudeep_blog_login = "login"
 		user_cookie = self.request.cookies.get("user_id")
 		if user_cookie and sudeep_blog_hasher.is_valid_cookie(user_cookie):
