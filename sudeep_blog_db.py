cdc0bd5575a7c772a5ab91f42a5c0536456d53c5
Sudeep Juvekar <sjuvekar@gmail.com>
2012-07-02 18:34:15 -0400

    adding maps and geolocation

--- sudeep_blog_db.py
+++ sudeep_blog_db.py
@@ -4,3 +4,4 @@ class SudeepBlogDB(db.Model):
 	sudeep_blog_db_subject = db.TextProperty(required = True)
 	sudeep_blog_db_body = db.TextProperty(required = True)
 	sudeep_blog_created = db.DateTimeProperty(auto_now_add = True)
+	sudeep_blog_coords = db.GeoPtProperty()
--- sudeep_blog_post.py
+++ sudeep_blog_post.py
@@ -1,12 +1,29 @@
 from sudeep_blog_template import SudeepBlogTemplate
 from sudeep_blog_db import SudeepBlogDB
+from google.appengine.ext import db
 import sudeep_blog_hasher
+import urllib2
+from xml.dom import minidom
 
 __SUDEEP_BLOG_POST_HTML__ = "sudeep_blog_post.html"
+__HTTP_INFO_STRING__ = "http://api.hostip.info/?ip="
 
 class SudeepBlogPostHandler(SudeepBlogTemplate):
   def render_blog(self, template, subject="", subject_error="", body="", body_error=""):
 	self.render(template, sudeep_blog_subject=subject, sudeep_blog_subject_error=subject_error, sudeep_blog_body=body, sudeep_blog_body_error=body_error)
+
+  def _get_coords(self, location_ip):
+	if not location_ip:
+		return None
+ 	ip_xml_string = __HTTP_INFO_STRING__ + location_ip
+	xml = urllib2.urlopen(ip_xml_string).read()
+	if not xml:
+		return None
+	xml_object = minidom.parseString(xml)
+	coords_tag = xml_object.getElementsByTagName("gml:coordinates")
+	if coords_tag and len(coords_tag) > 0 and coords_tag[0].childNodes and len(coords_tag[0].childNodes) > 0:
+		longitude, lattitude = coords_tag[0].childNodes[0].nodeValue.split(",")
+		return lattitude, longitude	
 
   def get(self):
 	user_cookie = self.request.cookies.get("user_id")
@@ -28,7 +45,12 @@ class SudeepBlogPostHandler(SudeepBlogTemplate):
 			body_error = "Body can not be empty. Please add some text to your blog"
 		self.render_blog(__SUDEEP_BLOG_POST_HTML__, subject, subject_error, body, body_error)
 	else:
-		db_entry = SudeepBlogDB(sudeep_blog_db_subject = subject, sudeep_blog_db_body = body)
+		location_ip = self.request.remote_addr
+		coords = self._get_coords(location_ip)
+		if coords:
+			db_entry = SudeepBlogDB(sudeep_blog_db_subject = subject, sudeep_blog_db_body = body, sudeep_blog_coords = db.GeoPt(coords[0], coords[1]))
+		else:	
+			db_entry = SudeepBlogDB(sudeep_blog_db_subject = subject, sudeep_blog_db_body = body)
 		db_entry.put()
 		self.redirect("/blog/{0}".format(db_entry.key().id()))
 
